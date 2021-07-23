5b1ce4628b41f2866f8e9539b4e39a16cc97ae0b
Dmitry <divol13@gmail.com>
2021-07-23 21:22:04 +0300

    Предварительно создается группа, если ее нет, в тестах удаления и модификации

--- addressbook-web-tests/src/test/java/ru/divol13/pft/addressbook/tests/DeleteGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/divol13/pft/addressbook/tests/DeleteGroupTests.java
@@ -1,12 +1,17 @@
 package ru.divol13.pft.addressbook.tests;
 
 import org.testng.annotations.*;
+import ru.divol13.pft.addressbook.model.GroupData;
 
 public class DeleteGroupTests extends TestBase {
 
   @Test
   public void testDeleteGroup() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+
+    if(!app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+    }
 
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroup();
