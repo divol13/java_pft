9b38fcc9a99289255bc122ff12a641434b12c631
Dmitry <divol13@gmail.com>
2021-09-05 20:02:44 +0300

    + убрал комменты

--- addressbook-web-tests/src/test/java/ru/divol13/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/divol13/pft/addressbook/tests/GroupModificationTests.java
@@ -12,14 +12,6 @@ public class GroupModificationTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions(){
-        /*
-        app.goTo().groupPage();
-        if (app.group().all().size() == 0) {
-            GroupData group = new GroupData().withName("test1").withHeader("test2").withFooter("test3");
-            app.group().create(group);
-        }
-        */
-
         if (app.db().groups().size() == 0) {
             app.goTo().groupPage();
             GroupData group = new GroupData().withName("test1").withHeader("test_header_1").withFooter("test_footer1");
@@ -29,7 +21,6 @@ public class GroupModificationTests extends TestBase {
 
     @Test
     public void testGroupModification() {
-        //Groups before = app.group().all();
         Groups before = app.db().groups();
         GroupData modifiedGroup = before.iterator().next();
 
@@ -43,7 +34,6 @@ public class GroupModificationTests extends TestBase {
         app.group().modify(group);
         assertThat(app.group().count(), equalTo(before.size()));
 
-        //Groups after = app.group().all();
         Groups after = app.db().groups();
         assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
     }
